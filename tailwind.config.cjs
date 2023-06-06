5a39f0898a59ea9dd65ced6c1636de836b228cf3
Sergio Perez <sergio-luis98@hotmail.com>
2023-06-06 16:01:17 -0500

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#0f0',
+				secondary: '#00f',
+				white: '#fff',
+			},
+			fontFamily: {
+				sans: ['GothamBold, sans-serif']
+			},
+		},
+	},
+	plugins: [],
+}
